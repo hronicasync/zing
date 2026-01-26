bf9bd1bc62c3f65bbca201d51888975b4042eaaf
elia hanumatori <vanheroper@gmail.com>
2026-01-26 23:51:37 +0300

    Initial Commit

---
+++ ZingNew/ZingNewApp.swift
@@ -0,0 +1,17 @@
+//
+//  ZingNewApp.swift
+//  ZingNew
+//
+//  Created by 067 on 26.01.2026.
+//
+
+import SwiftUI
+
+@main
+struct ZingNewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ZingNewTests/ZingNewTests.swift
@@ -0,0 +1,17 @@
+//
+//  ZingNewTests.swift
+//  ZingNewTests
+//
+//  Created by 067 on 26.01.2026.
+//
+
+import Testing
+@testable import ZingNew
+
+struct ZingNewTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
